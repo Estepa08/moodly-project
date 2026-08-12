1300e1099903532c4984e8881f7599567e3612a5
Evgeny_Stepanov <step.evgeny@gmail.com>
2026-08-12 18:32:09 +0300

    style: fix formatting in UI components

--- frontend/src/components/ui/toggle-switch.tsx
+++ frontend/src/components/ui/toggle-switch.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { cn } from '../../lib/utils';
 
-export interface ToggleSwitchProps
-  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'onChange' | 'checked'> {
+export interface ToggleSwitchProps extends Omit<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  'onChange' | 'checked'
+> {
   checked: boolean;
   onCheckedChange?: (next: boolean) => void;
   size?: 'sm' | 'md';
